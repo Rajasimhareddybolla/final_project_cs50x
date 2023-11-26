b907ee6369d66dbe12fef27044095318dbe45c65
raja <rajasimhabolla@gmail.com>
2023-11-26 12:12:32 +0530

    my final project
    i completed it sucessfully

--- app.py
+++ app.py
@@ -6,6 +6,18 @@ from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import apology, login_required, lookup, usd
 import datetime
+top_10_genres = [
+    "Literary Fiction",
+    "Mystery",
+    "Science Fiction (Sci-Fi)",
+    "Fantasy",
+    "Thriller",
+    "Biography",
+    "Self-Help",
+    "Romance",
+    "Historical Fiction",
+    "Non-Fiction"
+]
 
 # Configure application
 app = Flask(__name__)
@@ -16,9 +28,9 @@ app.jinja_env.filters["usd"] = usd
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
+page=0
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///bookmart.db")
 
 user_names = db.execute("SELECT username as name from users")
 
@@ -30,142 +42,87 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
+@app.route("/show_preview",methods=["GET","POST"])
+@login_required
 
+def show_preview():
+    if request.method =="GET":
+        return apology("sorry error")
+    image_url = request.form.get("image")
+    book = db.execute("select * from books where image=?",image_url)
 
+    gener= book[0]["gener"].split()
+    return render_template("preview.html",book=book[0],gener=gener)
+books_c={"Literary":[],"Mystery":[],"Science":[],"Fantasy":[],    "Thriller":[],    "Biography":[],    "Self-Help":[],
+ "Romance":[],    "Historical":[],    "Non-Fiction":[]}
+@app.route("/del",methods=["GET","POST"])
+@login_required
+def dele():
+    if request.method =="GET":
+        image_url=request.args.get("image")
+        name = db.execute("select bookname from books where image=?",image_url)
+        namee= name[0]["bookname"]
+        db.execute("delete from history where user_id=? and book_name=?",session["user_id"],namee)
+        return redirect("/cart")
+    else:
+        image_url = request.form.get("image")
+        book = db.execute("delete from books where image=?",image_url)
+        return redirect("/login")
+    
+@app.route("/Categories")
+def show_catogeries():
+    pres_stock = db.execute("select * from books")
+    return render_template("Categories.html")
 @app.route("/")
 @login_required
 def index():
-    # try:
-    #     db.execute("CREATE TABLE stocks(user_id INTEGER ,stock_name VARCHAR(255),stock_no INTEGER,transaction_type varchar(255),transaction_date varchar(255),status integer);")
-    # except:
-    #     pass
-    db.execute(f"drop table present{session['user_id']}")
-    id = session["user_id"]
-    try:
-        db.execute(
-            f"CREATE TABLE present{id} AS SELECT * FROM stocks where user_id = ? group by stock_name;",
-            999999,
-        )
-    except:
-        pass
+
     pres_stocks = db.execute(
-        "select stock_name,transaction_type,stock_no from stocks where user_id =?;", id
-    )
-    li_went = []
-    for i in range(0, len(pres_stocks)):
-        stock_name = pres_stocks[i]["stock_name"]
-        total_stocks = 0
-        if not (stock_name in li_went):
-            for k in range(0, len(pres_stocks)):
-                if stock_name == pres_stocks[k]["stock_name"]:
-                    if pres_stocks[k]["transaction_type"] == "BUY":
-                        total_stocks += int(pres_stocks[k]["stock_no"])
-                    else:
-                        total_stocks -= int(pres_stocks[k]["stock_no"])
-
-            db.execute(
-                f"insert into present{id}(user_id,stock_name,stock_no) values (?,?,?);",
-                id,
-                stock_name,
-                total_stocks,
-            )
-        li_went.append(stock_name)
-    remaining = db.execute("SELECT cash FROM users where id =?;", session["user_id"])
-    present = {}
-    active = []
-    stock_price = 0
-    stocks = db.execute(f"select * from present{id};")
-    for stock in stocks:
-        if stock["status"] != 0:
-            active.append(stock)
-            look = lookup(stock["stock_name"])
-            stock_price += look["price"] * stock["stock_no"]
-            present[stock["stock_name"]] = int(look["price"])
+        f"select bookname,price,author,image,publisher,description,gener from books ;")
+    id= session["user_id"]
+    user_prefered = db.execute("select preferd from users where id = ?",id)
+    geners_user=user_prefered[0]["preferd"].split()
+    dic ={}
+    for book in pres_stocks:
+        book_geners = book["gener"].split(" ")
+        gener_no = 0
+        for k in geners_user:
+            if k in book_geners:
+                gener_no+=1
+        dic[book["bookname"]]=gener_no
+    final_sorted = []
+    sorted_dict = dict(sorted(dic.items(), key=lambda item: item[1], reverse=True))
+    for bookname in sorted_dict.keys():
+        for k in pres_stocks:
+            if bookname == k["bookname"]:
+                final_sorted.append(k)
     return render_template(
-        "index.html",
-        stocks=active,
-        present=present,
-        re=remaining[0]["cash"],
-        price=round(stock_price, 3),
-        total=remaining[0]["cash"] + stock_price,
-    )
-
-
-@app.route("/buy", methods=["GET", "POST"])
+        "home.html",stocks=final_sorted)
+@app.route("/addbook",methods=["GET","POST"])
 @login_required
-def buy():
-    """Buy shares of stock"""
-    if request.method == "GET":
-        return render_template("buy.html")
-    else:
-        # try:
-        #     db.execute(f"drop table present{session['user_id']};")
-        # except:
-        #     pass
-        # try:
-        #     db.execute(f"CREATE TABLE present{session['user_id']} AS SELECT * FROM stocks where user_id = ? group by stock_name having ;",session['user_id'])
-        # except:
-        #     pass
-        if not (request.form.get("symbol") or request.form.get("shares")):
-            return apology("sorry enter valid input")
-        stock = request.form.get("symbol").upper()
-        stock_no = request.form.get("shares")
-        if not stock_no.isdigit():
-            return apology("its wrong", 400)
-        try:
-            stock_no = int(stock_no)
-        except:
-            return apology("its wrong", 400)
-        stock_details = lookup(stock)
-        if not stock_details:
-            return apology("not a valid stock")
-        cash = db.execute("SELECT cash FROM users WHERE id =?", session["user_id"])
-        ammount = stock_details["price"] * stock_no
-        if ammount > cash[0]["cash"]:
-            return apology("insufficient balance")
-        # try:
-        #     db.execute("CREATE TABLE stocks(user_id INTEGER ,stock_name VARCHAR(255),stock_no INTEGER,transaction_type varchar(255),transaction_date varchar(255),status integer);")
-        # except:
-        #     pass
-        db.execute(
-            "UPDATE users SET cash = (?) where id = (?);",
-            int(cash[0]["cash"] - ammount),
-            session["user_id"],
-        )
-        # use status = 0 for sold out and status =1 for right with us
-        try:
-            db.execute(
-                "INSERT INTO stocks(user_id,stock_name,stock_no,transaction_type,transaction_date,status) VALUES (?,?,?,?,?,?);",
-                session["user_id"],
-                stock,
-                stock_no,
-                "BUY",
-                datetime.datetime.now(),
-                1,
-            )
-            return redirect("/")
-        except:
-            return apology("some thing went wrong")
-
-
-@app.route("/history")
-@login_required
-def history():
-    """Show history of transactions"""
-    his = db.execute(
-        "select * from stocks where user_id = ? order by transaction_date DESC",
-        session["user_id"],
-    )
-    return render_template("history.html", data=his)
-
+def add():
+    id = session["user_id"]
+    title = request.form.get("title").capitalize()
+    author = request.form.get("author")
+    gener = request.form.getlist("types")
+    generee=""
+    for gen in gener:
+        generee+=gen
+        generee+=" "
+    price = request.form.get("price")
+    img = request.form.get("url")
+    discription = request.form.get("description")
+    publisher = db.execute("select * from publisher where id =?",id)
+    publisher=publisher[0]["username"]
+    db.execute("insert into books(bookname,price,author,image,gener,publisher,description) values (?,?,?,?,?,?,?);" ,title,price,author,img,generee,publisher,discription)
+    return redirect("/login")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
-
     # Forget any user_id
     session.clear()
-
+    page = 0
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
         # Ensure username was submitted
@@ -177,27 +134,36 @@ def login():
             return apology("must provide password", 403)
 
         # Query database for username
-        rows = db.execute(
-            "SELECT * FROM users WHERE username = ?", request.form.get("username")
-        )
-
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(
-            rows[0]["hash"], request.form.get("password")
-        ):
-            return apology("invalid username and/or password", 403)
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
-        try:
-            db.execute(
-                f"CREATE TABLE present{session['user_id']} AS SELECT * FROM stocks where user_id = ? group by stock_name;",
-                session["user_id"],
+        if request.form.get("type")=="user":
+            rows = db.execute(
+                "SELECT * FROM users WHERE mail = ?", request.form.get("username")
             )
-        except:
-            pass
-        # Redirect user to home page
-        return redirect("/")
-    # User reached route via GET (as by clicking a link or via redirect)
+            if len(rows) != 1 or not check_password_hash(
+                rows[0]["hash"], request.form.get("password")
+            ):
+                return apology("invalid username and/or password", 301)
+            # Remember which user has logged in
+            
+            session["user_id"] = rows[0]["id"]
+            # Redirect user to home page
+            return redirect("/")
+                # Ensure username exists and password is correct
+        else:
+            rows = db.execute(
+                "SELECT * FROM publisher WHERE mail = ?", request.form.get("username")
+            )
+            if len(rows) != 1 or not check_password_hash(
+                rows[0]["hash"], request.form.get("password")
+            ):
+                return apology("invalid username and/or password", 301)
+            # Remember which user has logged in
+            
+            session["user_id"] = rows[0]["id"]
+            name=db.execute("select username from publisher where id=?",session["user_id"])
+            books=db.execute("select * from books where publisher=?",name[0]["username"])
+            return render_template("publisher.html",geners=top_10_genres,books=books)
+            # Ensure username exists and password is correct
+    # Ur reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
 
@@ -209,34 +175,54 @@ def logout():
     session.clear()
     # Redirect user to login form
     return redirect("/")
+@app.route("/cart",methods=["GET","POST"])
+def add_cart():
+    if request.method=="POST":
+        item = request.form.get("image")
+        book = db.execute("select * from books where image =?",item)[0]["bookname"]
+        boo = db.execute("select book_name from history where user_id=?",session["user_id"])
+        exist = False
+        for bo in boo:
+            if bo["book_name"]==book:
+                exist = True
+        if  not exist:
+            db.execute("insert into history (user_id,book_name,transaction_date ) values (?,?,?);",session["user_id"],book,datetime.datetime.now())
+        else:
+            pass
+        return redirect("/cart")
+    else:
+        prodouct = db.execute("select * from history where user_id =?",session["user_id"])
+        books=[]
+        count=0
+        for prod in prodouct:
 
-
-@app.route("/quote", methods=["GET", "POST"])
-@login_required
-def quote():
-    """Get stock quote."""
-    if request.method == "GET":
-        return render_template("qoute.html")
-    stock = request.form.get("symbol")
-    if not request.form.get("symbol"):
-        return apology("Stocks not avaible")
-    results = lookup(stock)
-    if not results:
-        return apology("not found ", 400)
-    return render_template(
-        "qouted.html", name=results["name"], price=round(results["price"], 2)
-    )
-
-
+            name = prod["book_name"]
+            boo =db.execute("select * from books where bookname=?",name)
+            if len(boo)==0:
+                pass
+            else:
+                books.append(boo)
+                count+=1
+        return render_template("cart.html",prodoucts = books)
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user_completed"""
     if request.method == "GET":
-        return render_template("register.html")
+        return render_template("register.html",geners=top_10_genres)
     if request.method == "POST":
-        name = request.form.get("username")
+        fname = request.form.get("fname")
+        lname = request.form.get("lname")
+        name = fname+" "+lname
+        age = request.form.get("age")
+        age=int(age)
+        mail = request.form.get("mail")
+        gender  = request.form.get("gender")
+        prefered = request.form.getlist("types")
         password = request.form.get("password")
         confirmation = request.form.get("confirmation")
+        gener=""
+        for genr in prefered:
+            gener +=" "+genr
         if not (name or password or confirmation):
             return apology("try filling all")
         if not request.form.get("password"):
@@ -248,76 +234,30 @@ def register():
         #         return apology("SORRY , user already exist login kindley",400)
         if password != confirmation:
             return apology("mismatch of passwords")
-        try:
-            db.execute(
-                "INSERT INTO users(username,hash) VALUES (?,?)",
-                name,
-                generate_password_hash(password),
-            )
-        except:
-            return apology("sorry user already exist")
-        return redirect("/")
-    return apology("checking")
-
-
-@app.route("/sell", methods=["GET", "POST"])
-@login_required
-def sell():
-    """Sell shares of stock"""
-    if request.method == "GET":
-        id = session["user_id"]
-        user = db.execute("SELECT * FROM users WHERE id = ?;", id)
-        present = {}
-        active = []
-        stocks = db.execute(f"select * from present{id}")
-        stock_price = 0
-        for stock in stocks:
-            if stock["status"] != 0:
-                active.append(stock)
-                look = lookup(stock["stock_name"])
-                stock_price += look["price"]
-                present[stock["stock_name"]] = round(look["price"], 1)
-        remaining = db.execute("SELECT cash FROM users where id =?", session["user_id"])
-        return render_template(
-            "sell.html",
-            stocks=active,
-            present=present,
-            re=remaining[0]["cash"],
-            price=stock_price,
-        )
-    if request.method == "POST":
-        sale = request.form.get("symbol")
-        no = int(request.form.get("shares"))
-        id = session["user_id"]
-        stocks = db.execute(
-            "SELECT * FROM stocks WHERE user_id=? group by stock_name order by transaction_date DESC ;",
-            id,
-        )
-        present = db.execute(
-            f"SELECT stock_name,stock_no from present{session['user_id']};"
-        )
-        for sa in present:
-            if sale == sa["stock_name"]:
-                if no < 0 or no > sa["stock_no"]:
-                    return apology("its not the right way")
+        if request.form.get("type")=="user":
+            try:
                 db.execute(
-                    "INSERT INTO stocks(user_id,stock_name,stock_no,transaction_type,transaction_date,status) VALUES (?,?,?,?,?,?);",
-                    session["user_id"],
-                    sale,
-                    no,
-                    "SELL",
-                    datetime.datetime.now(),
-                    0,
+                    "INSERT INTO users(username,hash,age,mail,gender,preferd) VALUES (?,?,?,?,?,?)",
+                    name,
+                    generate_password_hash(password),
+                    age,
+                    mail,
+                    gender,
+                    gener
+
                 )
-                money = lookup(sa["stock_name"])
-                money_got = money["price"] * no
-                money_lis = db.execute(
-                    "select cash from users where id =?;", session["user_id"]
-                )
-                money_have = money_lis[0]["cash"]
+            except:
+                return apology("sorry user already exist")
+        else:
+            try:
                 db.execute(
-                    "update users set cash =? where id = ?;",
-                    money_have + money_got,
-                    session["user_id"],
+                    "INSERT INTO publisher(username,hash,age,mail) VALUES (?,?,?,?)",
+                    name,
+                    generate_password_hash(password),
+                    age,
+                    mail
                 )
-        return redirect("/")
+            except:
+                return apology("email already exist")
+        return redirect("/login")
+    return apology("Ran into a problem ",200)
